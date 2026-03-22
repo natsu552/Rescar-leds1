3a4ff69f99a0687ce39da93c567d2a4b8c0bca4f
natsu552 <kaidothegod20@gmail.com>
2026-03-22 15:54:27 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+
 import HomePage from '@/pages/HomePage'
 import ProductsPage from '@/pages/ProductsPage'
 import CartPage from '@/pages/CartPage'
@@ -10,6 +12,8 @@ import LoginPage from '@/pages/LoginPage'
 import AdminPage from '@/pages/AdminPage'
 import AccountPage from '@/pages/AccountPage'
 import SearchPage from '@/pages/SearchPage'
+import ContatoPage from '@/pages/ContatoPage' // 🔥 IMPORTANTE
+
 import ProtectedRoute from '@/components/ProtectedRoute'
 import { CartProvider } from '@/context/CartContext'
 
@@ -18,18 +22,29 @@ function App() {
     <Router>
       <CartProvider>
         <div className="min-h-screen bg-[#0A0A0A] flex flex-col">
+          
           <Navbar />
+
           <main className="flex-1">
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/produtos" element={<ProductsPage />} />
               <Route path="/carrinho" element={<CartPage />} />
               <Route path="/checkout" element={<CheckoutPage />} />
-              <Route path="/login" element={<LoginPage />} /><Route />
-<Route path="/admin" element={<AdminPage />} />
+              
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/admin" element={<AdminPage />} />
+
+              {/* 🔥 NOVAS ROTAS */}
+              <Route path="/conta" element={<AccountPage />} />
+              <Route path="/busca" element={<SearchPage />} />
+              <Route path="/contato" element={<ContatoPage />} />
+
             </Routes>
           </main>
+
           <Footer />
+
         </div>
       </CartProvider>
     </Router>
