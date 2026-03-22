136f889c244f3c4d2190c56fc1b1efaa08c258fb
natsu552 <kaidothegod20@gmail.com>
2026-03-22 10:17:15 -0300

    a

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   const fetchProducts = async () => {
     const { data, error } = await supabase
       .from('products')
-      .select('name, price, sale_price, image, featured')
+      .select(' image, featured')
 
     if (error) {
       console.log(error)
