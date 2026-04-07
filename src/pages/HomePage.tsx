1fabdee9a72501f7cd32265c05778e32ea90deb7
natsu552 <kaidothegod20@gmail.com>
2026-04-07 08:33:59 -0300

    a

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -130,7 +130,7 @@ export default function HomePage() {
                   )}
 
                   {/* 🔥 CARD ORIGINAL */}
-                  <ProductCard product={product} />
+                
 
                 </motion.div>
               )
