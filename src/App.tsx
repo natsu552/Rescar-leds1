887573cb7b4aa9313f44f15e5e20ffb3e10c71b4
natsu552 <kaidothegod20@gmail.com>
2026-04-07 07:30:57 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HashRouter as Router } from 'react-router-dom'
-import Navbar from '../components/Navbar'
+import Navbar from './components/Navbar'
 import Footer from '@/components/Footer'
 
 import HomePage from '@/pages/HomePage'
